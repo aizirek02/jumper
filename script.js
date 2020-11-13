aedf209b02eea3be0276b9ca92704168821996e5
DESKTOP-4QBKV91\Lenovo <aizi.zhuma@gmail.com>
2020-11-13 23:59:40 +0300

    Changed background and added rabbit and apple.

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let character = document.getElementById("character");
 let block = document.getElementById("block");
 var inputText = document.getElementById("inputText");
 var restart = document.getElementById("restart");
+
 inputText.innerHTML = "0";
-var blockAnimate = attack();
+var blockAnimates = attack();
 
 function refresh() {
     location.reload();
@@ -12,32 +13,50 @@ function refresh() {
 
 
 function jump() {
+    character.style.backgroundImage = "url('img/jump2.png')";
+    setTimeout(function () {
+        character.style.backgroundImage = "url('img/chrc_idle.png')";
+    }, 700);
     character.animate({
-        "top": ["150px", "20px", "150px"] // [ from, to ]
+        "top": ["210px", "30px", "210px"],
+        offset: [0, 0.6, 1],
+        easing: ['ease-out', 'ease-in'],
     }, {
         duration: 700,
         iterations: 1
     });
 }
 
+
 function attack() {
-    return block.animate({
-        "left": ["480px", "0px"]
-    }, {
-        duration: 1000,
-        iterations: Infinity
-    });
+    return [
+        block.animate({
+            "left": ["740px", "0px"],
+        }, {
+            duration: 1800,
+            iterations: Infinity
+        }),
+        block.animate({
+            "transform": 'rotate(-360deg)'
+        }, {
+            duration: 700,
+            iterations: Infinity
+        })
+
+    ]
 }
 
 let score = 0;
 let isWin = false;
 
 var loop = setInterval(() => {
-    if (parseInt(getCssVal(character, "left")) >= parseInt(getCssVal(block, "left")) - 20 &&
+    if (parseInt(getCssVal(character, "left")) >= parseInt(getCssVal(block, "left")) - 65 &&
+        parseInt(getCssVal(character, "left")) <= parseInt(getCssVal(block, "left")) + 65 &&
         parseInt(getCssVal(character, "top")) >= (parseInt(getCssVal(block, "top")) + 20)) {
-        console.log("You lose");
         clearInterval(loop);
-        blockAnimate.cancel();
+        blockAnimates[0].cancel();
+        blockAnimates[1].cancel();
+        restart.style.visibility = "visible";
     } else if (parseInt(getCssVal(character, "left")) >= parseInt(getCssVal(block, "left"))) {
         isWin = true;
     } else {
